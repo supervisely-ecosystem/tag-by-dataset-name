af18608a9ac8dba198d7bbc1ad83a72910aa40de
Nikolai Petukhov <petukhov.niko@gmail.com>
2022-12-13 13:25:59 -0300

    bug fix

--- src/main.py
+++ src/main.py
@@ -1,35 +1,41 @@
 import os
+from typing import Literal
 from dotenv import load_dotenv
 import supervisely as sly
 from supervisely._utils import batched
 from supervisely.annotation.tag_collection import TagCollection
 
 
-def tag_dataset(
+def get_or_create_tag_meta(
+    api: sly.Api,
+    project_meta: sly.ProjectMeta,
+    tag_name: str,
+    tag_value_type: str,
+):
+    tag_meta = project_meta.get_tag_meta(tag_name)
+    if tag_meta is None:
+        tag_meta = sly.TagMeta(tag_name, tag_value_type)
+        project_meta = project_meta.add_tag_meta(tag_meta)
+        api.project.update_meta(dataset.project_id, project_meta)
+        sly.logger.info(f'Added TagMeta "{tag_name}" with ValueType "{tag_value_type}"')
+        project_meta_json = api.project.get_meta(dataset.project_id)
+        project_meta = sly.ProjectMeta.from_json(project_meta_json)
+        tag_meta = project_meta.get_tag_meta(tag_name)
+    if tag_meta.value_type != tag_value_type:
+        raise ValueError(
+            f'TagMeta value type is not "{sly.TagValueType.NONE}". Wrong TagValueType: "{tag_meta.value_type}"'
+        )
+    return tag_meta
+
+
+def add_tag_to_dataset(
     api: sly.Api,
     dataset: sly.DatasetInfo,
-    project_id: int,
     project_meta: sly.ProjectMeta,
+    tag_meta: sly.TagMeta,
     batch_size: int = 100,
     progress: sly.Progress = None,
 ):
-    """Adds dataset name tag to all images in dataset"""
-
-    # Get or Create TagMeta
-    tag_meta = project_meta.get_tag_meta(dataset.name)
-    if tag_meta is None:
-        tag_meta = sly.TagMeta(dataset.name, sly.TagValueType.NONE)
-        project_meta = project_meta.add_tag_meta(tag_meta)
-        api.project.update_meta(project_id, project_meta)
-        project_meta_json = api.project.get_meta(project_id)
-        project_meta = sly.ProjectMeta.from_json(project_meta_json)
-        tag_meta = project_meta.get_tag_meta(dataset.name)
-    if tag_meta.value_type != sly.TagValueType.NONE:
-        sly.logger.warning(
-            f'TagMeta already exist in ProjectMeta but TagValueType is not "{sly.TagValueType.NONE}". Wrong TagValueType: "{tag_meta.value_type}"'
-        )
-        return
-
     images = api.image.get_list(dataset.id)
     for batch in batched(images, batch_size):
         img_ids = []
@@ -46,6 +52,33 @@ def tag_dataset(
             progress.set_current_value(progress.current + len(batch))
 
 
+def tag_dataset(
+    api: sly.Api,
+    dataset: sly.DatasetInfo,
+    batch_size: int = 100,
+    progress: sly.Progress = None,
+):
+    """Adds dataset name tag to all images in dataset"""
+
+    # Get project_meta
+    project_meta_json = api.project.get_meta(dataset.project_id)
+    project_meta = sly.ProjectMeta.from_json(project_meta_json)
+
+    # Get or Create TagMeta
+    try:
+        tag_meta = get_or_create_tag_meta(
+            api, project_meta, dataset.name, sly.TagValueType.NONE
+        )
+    except ValueError:
+        sly.logger.warning(
+            f'TagMeta "{dataset.name}" already exist in ProjectMeta but TagValueType is not "{sly.TagValueType.NONE}". Wrong TagValueType: "{tag_meta.value_type}". Skipping dataset...'
+        )
+        return
+
+    # Add tag to all images in dataset
+    add_tag_to_dataset(api, dataset, project_meta, tag_meta, batch_size, progress)
+
+
 if __name__ == "__main__":
     if sly.is_development():
         load_dotenv("local.env")
@@ -53,8 +86,6 @@ if __name__ == "__main__":
 
     api: sly.Api = sly.Api.from_env()
     project_id = sly.env.project_id()
-    project_meta_json = api.project.get_meta(project_id)
-    project_meta = sly.ProjectMeta.from_json(project_meta_json)
 
     dataset_id = sly.env.dataset_id(False)
     if dataset_id is None:
@@ -64,12 +95,12 @@ if __name__ == "__main__":
             total_imgs += dataset.images_count
         progress = sly.Progress("Processing", total_imgs)
         for dataset in datasets:
-            tag_dataset(api, dataset, project_id, project_meta, progress=progress)
+            tag_dataset(api, dataset, progress=progress)
     else:
         dataset = api.dataset.get_info_by_id(dataset_id)
         total_imgs = dataset.images_count
         progress = sly.Progress("Processing", total_imgs)
-        tag_dataset(api, dataset, project_id, project_meta, progress=progress)
+        tag_dataset(api, dataset, progress=progress)
 
     if sly.is_production():
         task_id = sly.env.task_id()
